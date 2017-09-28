51e6a9098ce1a3a466506b28719027589ccd8427
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-09-28 14:34:38 +0200

    Add travis support
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

---
+++ config/buildDockerImage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+	docker build --no-cache -t $DOCKER_IMAGE_NAME .;
+	docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:build-$TRAVIS_BUILD_NUMBER;
+	docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:latest;
+	docker push $DOCKER_IMAGE_NAME:build-$TRAVIS_BUILD_NUMBER;
+	docker push $DOCKER_IMAGE_NAME:latest;
+fi
